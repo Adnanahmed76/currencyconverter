aee6b1d646a630e5500b4666294e48636ba60276
Adnan Ahmed <adnanahmed504423@gmail.com>
2024-07-24 23:59:44 +0530

    commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,6 +9,6 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
